33f295b315b8736f58c08ee56a56e47849dec4a4
Algenis E. Volquez <evolquez@gmail.com>
2015-09-13 03:58:14 -0400

    Adding login form and app user section. user info hardcoded

--- assets/js/application.js
+++ assets/js/application.js
@@ -14,9 +14,35 @@ var perstodo_app = angular.module('perstodo-app', ['ui.router']);
 
 /*Config route mapping with angularjs $routeProvider*/
 perstodo_app.config(function($stateProvider, $urlRouterProvider){
+	
+	//homepage
 	$stateProvider.state('home', {
 		url 		: '/',
 		templateUrl : 'views/home-page.html' 
+	});
+
+	//login
+	$stateProvider.state('login', {
+		url 		: '/app/login', 
+		templateUrl : 'views/app/login.html',
+		controller 	: 'loginCtrl'
+	});
+
+	//app main tamplate
+	$stateProvider.state('app', {
+		url 		: '/app', 
+		templateUrl : 'views/app/template.html'
+	});
+
+	//app dashboard
+	$stateProvider.state('app.dashboard', {
+		url 		: '/dashboard',
+		cache		: false, 
+		views		: {
+			'page-content' : {
+				templateUrl : 'views/app/dashboard.html'
+			}
+		}
 	});
 
 	$urlRouterProvider.otherwise('/');
---
+++ assets/js/controllers/login.ctrl.js
@@ -0,0 +1,37 @@
+ /**
+ * login.ctrl.js
+ * 
+ * This file contains the logic for manage users authentication to the app
+ * 
+ * @author Algenis E. Vólquez <evolquez@gmail.com>
+ * @date Sept 13th, 2015
+ */
+
+/**
+ * login_ctrl function
+ * @param object $scope
+ * @param object $timeout
+ * @param object $location
+ */
+function login_ctrl($scope, $timeout, $location){
+	$scope.btnLogin = 'Login';
+	$scope.logging = false;
+
+	$scope.login = function(user){
+		$scope.btnLogin = 'Logging in, please wait...';
+		$scope.logging = true;
+		
+		$timeout(function(){
+			$scope.btnLogin = 'Login';
+			$scope.logging = false;
+			if(user.email === 'admin@admin.com' && user.passw === 'admin'){
+				delete $scope.auth_error;
+				$scope.user.email = "";
+				$scope.user.passw = "";
+				$location.path('/app/dashboard'); //Redirect to dashboard
+			}else{
+				$scope.auth_error = true;
+			}
+		}, 800);
+	}
+}
